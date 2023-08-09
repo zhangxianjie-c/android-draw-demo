abcce78c63dc260b617763328afa9c8a01f5f180
zhangxianjie <xianjie.zhang@zebra-c.com>
2023-08-09 16:01:26 +0800

    common commit.

--- app/src/main/java/priscilla/draw/MainActivity.kt
+++ app/src/main/java/priscilla/draw/MainActivity.kt
@@ -3,13 +3,13 @@ package priscilla.draw
 import android.content.res.Resources
 import android.os.Bundle
 import android.util.TypedValue
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        theme
+        Toast.makeText(this, "111", Toast.LENGTH_SHORT).show()
     }
 }
